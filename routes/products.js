b653914543d3722624f0c64943edf3eb3bcc2521
Dai Nguyen <ncdai3651408@gmail.com>
2021-11-23 23:35:52 +0700

    Add product view page

--- routes/products.js
+++ routes/products.js
@@ -7,6 +7,10 @@ router.get('/', function (req, res) {
   res.render('products/index')
 })
 
+router.get('/view', function (req, res) {
+  res.render('products/view')
+})
+
 router.get('/create-category', productController.createCategory)
 
 module.exports = router
